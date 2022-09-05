00ac12af82976631e7d296622a2d0829becc061d
DevilKo0l <nguyen.tcao96@gmail.com>
2022-09-05 22:36:27 +0200

    update calculator HTML

--- calculator/script.js
+++ calculator/script.js
@@ -1,12 +1,41 @@
+//
+const numberButtons = document.querySelectorAll(".btn");
+const operationButtons = document.querySelectorAll("");
+
 //Display number after click
 const buttons = document.querySelectorAll(".btn");
-let arr = [];
+let arrInput = [];
 buttons.forEach((btn) => {
   btn.addEventListener("click", (event) => {
     console.log(event.target.textContent);
-    arr.push(event.target.textContent);
+    arrInput.push(event.target.textContent);
+    document.querySelector(".input").value = arrInput.join("");
+
+    if (event.target.textContent === "AC") {
+      arrInput = [];
+      document.querySelector(".input").value = arrInput;
+    }
+
+    if (event.target.textContent === "=") {
+    }
   });
 });
 
-document.querySelector(".input").textContent = arr.toString();
-console.log(arr.toString());
+function calculate(num1, num2, operationInput) {
+  if (operationInput == "+") {
+    return num1 + num2;
+  } else if (operationInput == "-") {
+    return num1 - num2;
+  } else if (operationInput == "*") {
+    return num1 * num2;
+  } else if (operationInput == "/") {
+    return num1 / num2;
+  } else if (operationInput == "^") {
+    return Math.pow(num1, num2);
+  } else {
+    return 0;
+  }
+}
+
+console.log(calculate(1, 2, "+"));
+console.log(2 + 3 * 5);
