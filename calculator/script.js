3df0e82b4f8c3c83bf9ba4efd285555cd3d867fc
DevilKo0l <nguyen.tcao96@gmail.com>
2022-09-07 22:08:51 +0200

    update calculator function

--- calculator/script.js
+++ calculator/script.js
@@ -1,13 +1,37 @@
-//
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
-const equalButton = document.querySelector("[data-equals]");
+const equalsButton = document.querySelector("[data-equals]");
+const allClearButton = document.querySelector("[data-all-clear]");
 
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
+
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+  }
+
+  clear() {}
+
+  delete() {}
+
+  appendNumber(number) {}
+
+  chooseOperation(operation) {}
+
+  compute() {}
+  updateDisplay() {}
+}
 //Display number after click
 const buttons = document.querySelectorAll(".btn");
 let arrInput = [];
-buttons.forEach((btn) => {
-  btn.addEventListener("click", (event) => {
+buttons.forEach((numberButtons) => {
+  numberButtons.addEventListener("click", (event) => {
     console.log(event.target.textContent);
     arrInput.push(event.target.textContent);
     document.querySelector(".input").value = arrInput.join("");
